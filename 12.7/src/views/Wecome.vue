323be3544ceae5daedf0b0daea6ad642bec37a80
“dyhove” <647547745@qq.com>
2022-12-07 23:12:06 +0800

    12.7

---
+++ 12.7/src/views/Wecome.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>WeCome</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  methods: {},
+  created() {},
+  mounted() {},
+  components: {},
+  computed: {},
+  watch: {}
+}
+</script>
+
+<style lang="scss" scoped></style>
